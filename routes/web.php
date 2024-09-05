d8e6f829d98f40be3c73ea19e97227bf832b6d37
Zahidi <ahmadzahidi120@gmail.com>
2024-09-05 17:04:28 +0800

    Sessions, Errors & Flash Messages

--- routes/web.php
+++ routes/web.php
@@ -7,35 +7,36 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/',function(){
+Route::get('/', function () {
   return redirect()->route('tasks.index');
 });
 
 Route::get('/tasks', function () {
-    return view('index',[
-        'tasks'=>Task::latest()->where('completed',true)->get()
-    ]);
+  return view('index', [
+    'tasks' => Task::latest()->where('completed', true)->get()
+  ]);
 })->name('tasks.index');
 
-Route::view('/tasks/create','create')->name('task.create');
+Route::view('/tasks/create', 'create')->name('task.create');
 
 Route::get('/tasks/{id}', function ($id) {
   return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
-Route::post('/tasks',function(Request $request){
-  $data=$request->validate([
-    'title'=>'required|max:255',
-    'description'=>'required',
-    'long_description'=>'required'
+Route::post('/tasks', function (Request $request) {
+  $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
   ]);
 
-  $task=new Task;
-  $task->title=$data['title'];
-  $task->description=$data['description'];
-  $task->long_description=$data['long_description'];
+  $task = new Task;
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
 
   $task->save();
 
-  return redirect()->route('tasks.show',['id'=>$task->id]);
+  return redirect()->route('tasks.show', ['id' => $task->id])
+    ->with('success', 'Task created successfully');
 })->name('tasks.store');
